faee4ecbf70e10036ff0177d23a1d13567b77571
richard-dinh <richarddinh@gmail.com>
2020-01-29 19:29:05 -0800

    wrote save appointment function

--- Develop/app.js
+++ Develop/app.js
@@ -1,17 +1,15 @@
-
-let nineText = $('#9')
-let nineBtn = $('#nineBtn')
-let nineHour = $('#nineHour')
 let appointment
-nineBtn.click(()=>{
-  appointment = {
-    text: `${nineText.val()}`,
-    time: `${nineHour.text()}`
-  }
-  //sets the key to be the id of the textera of that hour
-  localStorage.setItem(`${nineText.attr('id')}`,JSON.stringify(appointment))
-})
 
+const saveAppointment = id => {
+  id = parseInt(id)
+  id = id.toString()
+  let selector = $(`#${id}`)
+  appointment = {
+    text: `${selector.val()}`,
+    time: `${selector.text()}`
+  }
+  localStorage.setItem(`${selector.attr('id')}`, JSON.stringify(appointment))
+}
 const renderCalendar = _=>{
   for (let i = 9; i< 18; i++){
     let selector = $(`#${i}`)
@@ -20,10 +18,20 @@ const renderCalendar = _=>{
     if (JSON.parse(localStorage.getItem(key)) !==null){
       let temp = JSON.parse(localStorage.getItem(key))
       selector.text(temp.text)
-      console.log(temp.text)
     }
   }
 }
+$(document).click(_=>{
+  let target = $(event.target)
+  console.log($(event.target))
+  //checks if a button is clicked
+  if($(event.target).is('button')){
+    console.log('BUTTON')
+    console.log(target.attr('id'))
+    saveAppointment(target.attr('id'))
+  }
+})
 
 renderCalendar()
+
 
