8993215cf2fe6f5f5a44c6699c44d85206bb52c1
richard-dinh <richarddinh@gmail.com>
2020-01-29 20:05:27 -0800

    fixed render to show appointments correctly

--- Develop/app.js
+++ Develop/app.js
@@ -1,4 +1,15 @@
 let appointment
+const renderCalendar = () =>{
+  for (let i = 9; i < 18; i++) {
+    let selector = $(`#${i}`)
+    let key = i.toString()
+    //only outputs if there is a key
+    if (localStorage.getItem(key) !== null) {
+      let temp = JSON.parse(localStorage.getItem(key))
+      selector.val(temp.text)
+    }
+  }
+}
 
 const saveAppointment = id => {
   id = parseInt(id)
@@ -6,28 +17,18 @@ const saveAppointment = id => {
   let selector = $(`#${id}`)
   appointment = {
     text: `${selector.val()}`,
-    time: `${selector.text()}`
   }
+  console.log(selector.val())
+  console.log(selector.text())
   localStorage.setItem(`${selector.attr('id')}`, JSON.stringify(appointment))
+  renderCalendar()
 }
-const renderCalendar = _=>{
-  for (let i = 9; i< 18; i++){
-    let selector = $(`#${i}`)
-    let key = i.toString()
-    //only outputs if there is a key
-    if (JSON.parse(localStorage.getItem(key)) !==null){
-      let temp = JSON.parse(localStorage.getItem(key))
-      selector.text(temp.text)
-    }
-  }
-}
+
 $(document).click(_=>{
   let target = $(event.target)
   console.log($(event.target))
   //checks if a button is clicked
   if($(event.target).is('button')){
-    console.log('BUTTON')
-    console.log(target.attr('id'))
     saveAppointment(target.attr('id'))
   }
 })
